bb3d89c717b4a4fc56c02031c9db743a462bfd7f
shahzain <shahzainjaved97@gmail.com>
2023-12-15 01:52:16 +0500

    auditinglogs

---
+++ src/main/java/com/example/practiceJpa/auditingLogs/Auditing.java
@@ -0,0 +1,33 @@
+package com.example.practiceJpa.auditingLogs;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditing {
+    @CreatedBy
+    protected String createdBy;
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    protected Date createDate;
+
+    @LastModifiedBy
+    protected String lsatModify;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    protected  Date lastModifyDate;
+}
